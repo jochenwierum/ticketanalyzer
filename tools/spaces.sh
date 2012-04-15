c6ee205d9b0a064d19b4a0c50de0cc786f6474b3
Jochen Wierum <jochen@jowisoftware.de>
2012-04-16 00:46:25 +0200

    [DW] added support to be used as git hook

--- tools/spaces.sh
+++ tools/spaces.sh
@@ -3,17 +3,37 @@
 set -e
 cd "$(dirname "$(readlink -f $0)")"/..
 
+GIT=0
+if [ "$1" == "--git" ]; then
+	shift
+	GIT=1
+fi
+
 DOIT=0
 if [ "$1" == "--doit" ]; then
+	shift
 	DOIT=1
 fi
 
+result=1
 PATTERN='^\s*\t|[\t\s]{1,}$'
 if [ $DOIT -eq 1 ]; then
-	find -name '*.scala' | xargs grep -Prl $PATTERN | xargs perl -i.spacesfix -p -e 'if(/^(\s*\t[\s\t]*)/) {($s, $m) = ($1, $1); $s =~ s/\t/    /g; s/^$m/$s/}; s/[\t\s]{1,}([\n\r]+)/$1/; $_'
-	find -name '*.scala.spacesfix' | xargs rm
+	find -name '*.scala' -print0 | xargs -r0 grep -PrlZ $PATTERN | xargs -0 perl -i.spacesfix -p -e 'if(/^(\s*\t[\s\t]*)/) {($s, $m) = ($1, $1); $s =~ s/\t/    /g; s/^$m/$s/}; s/[\t\s]{1,}([\n\r]+)/$1/; $_'
+	find -name '*.scala.spacesfix' -print0 | xargs -r0 rm
+	result=0
 else
-	echo "This is only a dry run. Start this script with --doit to remove trailing spaces"
-	echo "Before you do it, make sure you did a commit"
-	find -name '*.scala' | xargs grep -Pr $PATTERN
+	if [ $GIT -eq 0 ]; then
+		echo "This is only a dry run. Start this script with --doit to remove trailing spaces"
+		echo "Before you do it, make sure you did a commit"
+	else
+		echo "Checking for illegal spaces..."
+	fi
+	set +e
+	find -name '*.scala' -print0 | xargs -r0 grep -Prn $PATTERN
+	if [ $? -eq 123 ]; then
+		result=0
+	fi
+	set -e
 fi
+
+exit $result
