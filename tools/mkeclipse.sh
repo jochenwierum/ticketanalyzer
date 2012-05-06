62926994a088adb7dd4cf114b297285c5635fd07
Jochen Wierum <jochen@jowisoftware.de>
2012-05-06 10:05:43 +0200

    [DW] finished migration to gradle

--- tools/mkeclipse.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-cd "$(dirname "$(readlink -f $0)")"/..
-tools/sbt eclipse
-
-find -name .classpath | xargs perl -i.orig -p -e 's#"(?:\.\.[\\/])*lib_managed(.*)kind="lib"#"PROJECT_LIBS$1kind="var"#'
-find -name .classpath.orig | xargs rm
--- tools/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$(readlink -f $0)")"/..
-exec tools/sbt package 'project ticketanalyzer-core' 'run-main de.jowisoftware.mining.Main'
