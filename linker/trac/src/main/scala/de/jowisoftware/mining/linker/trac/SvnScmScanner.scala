04990d284b9cafcdd786063d578e24d5dff0b472
Jochen Wierum <jochen@jowisoftware.de>
2012-07-11 11:31:53 +0200

    [DW] changed regex to support git commits

--- linker/trac/src/main/scala/de/jowisoftware/mining/linker/trac/SvnScmScanner.scala
+++ linker/trac/src/main/scala/de/jowisoftware/mining/linker/trac/SvnScmScanner.scala
@@ -5,19 +5,21 @@ import de.jowisoftware.mining.linker.ScmLink
 import scala.util.matching.Regex
 
 private[trac] object SvnScmScanner {
-  private val singleRegexes = List("""r(\d+)""".r,
-    """\[(\d+)(?:(/[^\]]+))?\]""".r,
-    """changeset:(\d+)(/[A-Za-z0-9/]+)?""".r)
+  private val singleRegexes = List("""r(\d+|[0-9a-fA-F]{1,32})""".r,
+    """\[(\d+|[0-9a-fA-F]{1,32})(?:(/[^\]]+))?\]""".r,
+    """changeset:(\d+|[0-9a-fA-F]{1,32})(/[A-Za-z0-9/]+)?""".r)
 
   private val rangeRegexes = List(new Regex("""r(\d+:\d+)""", "revs"),
-    new Regex("""\[(\d+:\d+)(/[^\]]+)?\]""", "revs", "path"),
-    new Regex("""log:([^\s]+)?@(\d+:\d+)""", "path", "revs"))
+    new Regex("""\[((?:\d+|[0-9a-fA-F]{1,32}):(?:\d+|[0-9a-fA-F]{1,32}))(/[^\]]+)?\]""", "revs", "path"),
+    new Regex("""log:([^\s]+)?@((?:\d+|[0-9a-fA-F]{1,32}):(?:\d+|[0-9a-fA-F]{1,32}))""", "path", "revs"))
+
+  private def isNumeric(s: String) = s.matches("""^\d+$""")
 }
 
 private[trac] class SvnScmScanner extends ScmScanner {
   import SvnScmScanner._
 
-  def scan(text: String) = {
+  def scan(text: String): Set[ScmLink] = {
     val singleMatches = singleRegexes.flatMap(_.findAllIn(text).matchData.map { theMatch =>
       if (theMatch.groupCount > 1 && theMatch.group(2) != null)
         ScmLink(theMatch.group(1), path = Some(theMatch.group(2)))
@@ -26,14 +28,17 @@ private[trac] class SvnScmScanner extends ScmScanner {
     }).toSet
 
     val rangeMatches = rangeRegexes.flatMap(_.findAllIn(text).matchData.flatMap { theMatch =>
-      val path = if (theMatch.groupCount == 2 && theMatch.group("path") != null) Some(theMatch.group("path")) else None
+      val path = if (theMatch.groupCount == 2 && theMatch.group("path") != null)
+        Some(theMatch.group("path"))
+      else
+        None
       makeRange(theMatch.group("revs"), path)
     }).toSet
 
     singleMatches ++ rangeMatches
   }
 
-  def makeRange(rangeSpec: String, path: Option[String]) = {
+  private def makeRange(rangeSpec: String, path: Option[String]): Seq[ScmLink] = {
     val rangeParts = rangeSpec.split(":")
     for (i <- rangeParts(0).toInt to rangeParts(1).toInt) yield ScmLink(i.toString, path = path)
   }
