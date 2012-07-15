56c225f7f908bef06f54d6044e6e9fb69102d3a4
Jochen Wierum <jochen@jowisoftware.de>
2012-07-15 18:13:06 +0200

    [DW] fixed tests for mock

--- linker/trac/src/test/scala/de/jowisoftware/mining/linker/trac/TicketScanner.scala
+++ linker/trac/src/test/scala/de/jowisoftware/mining/linker/trac/TicketScanner.scala
@@ -11,19 +11,19 @@ class TicketScannerTest extends FunSpec {
     assert(result === expected)
   }
 
-  private def links(ids: String*) = ids.toSet.map { id: String => TicketLink(id) }
+  private def links(ids: Int*) = ids.toSet.map { id: Int => TicketLink(id) }
 
   describe("A TicketScanner") {
     it("should find #1 and #4") {
-      check("Implemented #1 and #4", links("1", "4"))
+      check("Implemented #1 and #4", links(1, 4))
     }
 
     it("should find ticket:12 and ticket:26") {
-      check("Implemented ticket:26 and parts of ticket:12!", links("12", "26"))
+      check("Implemented ticket:26 and parts of ticket:12!", links(12, 26))
     }
 
     it("should support Mantis:12 and mantis: 14 (case insensitive with optional space)") {
-      check("Implemented Mantis:1, mantis:2, MantiS: 3 and Mantis: 4!", links("1", "2", "3", "4"))
+      check("Implemented Mantis:1, mantis:2, MantiS: 3 and Mantis: 4!", links(1, 2, 3, 4))
     }
   }
 }
