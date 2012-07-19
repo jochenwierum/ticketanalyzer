1eb25b528632b9480326dfb62d156e5fffef6699
Jochen Wierum <jochen@jowisoftware.de>
2012-07-19 16:05:15 +0200

    [DW] redesigned delete pane

--- core/src/main/scala/de/jowisoftware/mining/gui/DeletePane.scala
+++ core/src/main/scala/de/jowisoftware/mining/gui/DeletePane.scala
@@ -1,19 +1,27 @@
 package de.jowisoftware.mining.gui
 
-import scala.swing.BorderPanel
-import scala.swing.BoxPanel
-import scala.swing.Orientation
-import de.jowisoftware.neo4j.Database
+import scala.swing.{ Orientation, Label, Frame, Button, BoxPanel, Alignment }
 import scala.swing.event.ButtonClicked
-import scala.swing.Button
-import scala.swing.Frame
-import de.jowisoftware.mining.model.nodes.RootNode
+
 import de.jowisoftware.mining.gui.MainWindow.DatabaseUpdated
+import de.jowisoftware.mining.model.nodes.RootNode
+import de.jowisoftware.neo4j.Database
 
 class DeletePane(val db: Database[RootNode], parent: Frame)
     extends BoxPanel(Orientation.Vertical) with GuiTab {
 
-  val button = new Button("Drop database")
+  val label = new Label("""<html><p>
+      |<b>Drop the database</b><br />
+      |By clicking on &quot;yes, drop the database&quot; the whole database (the directory<br />
+      |&quot;db&quot; in the program installation) will be deleted. There is <i>no</i> way back!<br />
+      |You can backup the directory by simply copying it while the application<br />
+      |is not running.<br /><br />
+      |Are you shure you want to delete the database?
+      |</p></html>""".stripMargin)
+  label.horizontalAlignment = Alignment.Left
+
+  val button = new Button("Yes, drop the database")
+  contents += label
   contents += button
 
   listenTo(button)
