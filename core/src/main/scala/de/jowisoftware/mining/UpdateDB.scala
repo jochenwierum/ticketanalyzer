4894e78a844a9412fc7048a1486e6f1c70fea049
Jochen Wierum <jochen@jowisoftware.de>
2012-08-04 11:00:13 +0200

    [DW] finished update routine

--- core/src/main/scala/de/jowisoftware/mining/UpdateDB.scala
+++ core/src/main/scala/de/jowisoftware/mining/UpdateDB.scala
@@ -10,6 +10,8 @@ import scala.collection.JavaConversions._
 import de.jowisoftware.neo4j.content.Node
 import org.neo4j.jmx.Kernel
 import org.neo4j.graphdb.Direction
+import org.neo4j.graphdb.{ Node => NeoNode, Relationship => NeoRelationship }
+import de.jowisoftware.neo4j.content.Relationship
 
 object UpdateDB {
   def main(args: Array[String]) {
@@ -17,15 +19,32 @@ object UpdateDB {
     val dbPath = new File(AppUtil.basePath, settings.getString("db")).getCanonicalFile
     val db = new EmbeddedDatabase(dbPath, RootNode)
 
-    val count = GlobalGraphOperations.at(db.service).getAllNodes.size
+    println("Updating nodes...")
+    performUpgrade[NeoNode](db,
+      GlobalGraphOperations.at(db.service).getAllNodes,
+      n => Node.neoNode2Node(n, db))
+
+    println("Updating relationships...")
+    performUpgrade[NeoRelationship](db,
+      GlobalGraphOperations.at(db.service).getAllRelationships,
+      r => updateRelationship(r, db))
+
+    println("Update finished")
+    db.shutdown
+  }
+
+  private def performUpgrade[A](db: EmbeddedDatabase[RootNode],
+    collector: => Iterable[A], updateAction: A => Unit): Unit = {
+
+    val count = collector.size
     var i = 0L
     var transaction = db.service.beginTx
-    for (node <- GlobalGraphOperations.at(db.service).getAllNodes) {
-      Node.neoNode2Node(node, db) // TODO: check relationships also!
+    for (obj <- collector) {
+      updateAction(obj)
 
       i = i + 1
       if (i % 1000 == 0) {
-        println((100.0*i/count).formatted("%.2f")+" %: "+i+" of "+count+" Nodes...")
+        println((100.0 * i / count).formatted("%.2f")+" %: "+i+" of "+count+" Objects...")
         transaction.success
         transaction.finish
         transaction = db.service.beginTx
@@ -33,6 +52,11 @@ object UpdateDB {
     }
     transaction.success
     transaction.finish
-    println("Update finished")
+  }
+
+  private def updateRelationship(rel: NeoRelationship, db: EmbeddedDatabase[RootNode]) {
+    val className = rel.getProperty("_class").asInstanceOf[String]
+    val target = Class.forName(className).newInstance.asInstanceOf[Relationship]
+    target.initWith(rel, db)
   }
 }
