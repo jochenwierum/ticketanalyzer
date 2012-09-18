bcb9ee08c3cb6e9c3fcba21e0dc3b7ec52112208
Jochen Wierum <jochen@jowisoftware.de>
2012-09-18 11:23:56 +0200

    [DW] on high load, redmine seems to throw error 500 exceptions some time. In this case, the plugin tries 3 reloads bevore it gives up.

--- importer/redmine/src/main/scala/de/jowisoftware/mining/importer/redmine/RedmineClient.scala
+++ importer/redmine/src/main/scala/de/jowisoftware/mining/importer/redmine/RedmineClient.scala
@@ -1,15 +1,15 @@
 package de.jowisoftware.mining.importer.redmine
 
-import java.net.URLEncoder
-import java.net.URL
-import scala.xml.XML
+import java.io.{ IOException, InputStream }
+import java.net.{ URL, URLConnection, URLEncoder }
+
 import scala.annotation.tailrec
-import scala.collection.mutable
-import scala.xml.Elem
+import scala.xml.{ Elem, XML }
 
-import de.jowisoftware.util.XMLUtils._
+import de.jowisoftware.util.XMLUtils.NodeSeq2EnrichedNodeSeq
+import grizzled.slf4j.Logging
 
-class RedmineClient(redmineUrl: String, apikey: String) {
+class RedmineClient(redmineUrl: String, apikey: String) extends Logging {
   private val baseUrl = redmineUrl + (if (!redmineUrl.endsWith("/")) "/" else "")
 
   def retrieveXML(file: String, parameterMap: Map[String, String]) = {
@@ -22,7 +22,22 @@ class RedmineClient(redmineUrl: String, apikey: String) {
     val connection = rpcUrl.openConnection()
     connection.setRequestProperty("Content-Type", "application/xml")
     connection.setRequestProperty("X-Redmine-API-Key", apikey)
-    XML.load(connection.getInputStream())
+
+    XML.load(openInputStream(connection))
+  }
+
+  private def openInputStream(connection: URLConnection): InputStream = {
+    for (i <- 0 to 2) {
+      try {
+        val stream = connection.getInputStream()
+        return stream
+      } catch {
+        case e: IOException =>
+          warn("IOException while receiving page... retrying "+(i + 1), e)
+          Thread.sleep(2000)
+      }
+    }
+    throw new RuntimeException("Could not open connection to "+connection.getURL().toString())
   }
 
   def retrivePagedXML(file: String,
