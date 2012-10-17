36c27502ab7289b11e46eaeb31806f6012365204
Jochen Wierum <jochen@jowisoftware.de>
2012-10-17 15:59:28 +0200

    [BF] reporter were not counted because they don't own a ticket

--- analyzer/roles/src/main/scala/de/jowisoftware/mining/analyzer/roles/RolesAnalyzer.scala
+++ analyzer/roles/src/main/scala/de/jowisoftware/mining/analyzer/roles/RolesAnalyzer.scala
@@ -46,22 +46,41 @@ class RolesAnalyzer extends Analyzer {
     result
   }
 
+  private def findReporterCountByName(db: Database[RootNode]): ExecutionResult = {
+    val query = """
+        START persons=node(%d)
+        MATCH persons --> person -[:created]-> ticket
+        WHERE person.name <> ""
+        RETURN person.name AS name, count(distinct ticket.id) AS count
+        """ format (db.rootNode.personCollection.id)
+
+    val engine = new ExecutionEngine(db.service)
+    val result = engine.execute(query)
+    result
+  }
+
   private def buildStateMap(db: Database[RootNode]): Map[String, mutable.Map[StatusType.Value, Long]] = {
     var personStateCounter: Map[String, mutable.Map[StatusType.Value, Long]] = Map()
+
+    def addCount(name: String, state: StatusType.Value, count: Long) {
+      val stateMap = personStateCounter.getOrElse(name,
+        { personStateCounter += name -> mutable.Map(); personStateCounter(name) })
+
+      stateMap(state) = (stateMap.getOrElse(state, 0L) + count)
+    }
 
     for (stateWithName <- findStateCountByName(db)) {
       val person = stateWithName("name").asInstanceOf[String]
       val state = StatusType(stateWithName("status").asInstanceOf[Int])
       val count = stateWithName("count").asInstanceOf[Long]
+      addCount(person, state, count)
+    }
 
-      val stateMap = personStateCounter.getOrElse(person,
-        { personStateCounter += person -> mutable.Map(); personStateCounter(person) })
+    for (reporterWithCount <- findReporterCountByName(db)) {
+      val person = reporterWithCount("name").asInstanceOf[String]
+      val count = reporterWithCount("count").asInstanceOf[Long]
 
-      stateMap(state) = if (stateMap contains state) {
-        stateMap(state) + count
-      } else {
-        count
-      }
+      addCount(person, StatusType.reported, count)
     }
 
     personStateCounter
